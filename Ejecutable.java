0a88f494dab02a578c8ca1206f6f4e956737c6ec
Javier Gomez <jagomezmarco16@gmail.com>
2018-05-03 16:04:49 +0200

    first commit

---
+++ Ejecutable.java
@@ -0,0 +1,10 @@
+package ejercicioJGM;
+
+public class Ejecutable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Personas.java
@@ -0,0 +1,87 @@
+package ejercicioJGM;
+
+public class Personas {
+
+	private String colorOjos;
+	private String colorPelo;
+	private String tipoPelo;
+	private String tipoOjos;
+	private String nombre;
+	private int edad;
+	private String ciudad;
+	private double peso;
+	private double altura;
+
+	public String getColorOjos() {
+		return this.colorOjos;
+	}
+
+	public void setColorOjos(String _colorOjos) {
+		this.colorOjos = _colorOjos;
+	}
+
+	public String getColorPelo() {
+		return this.colorPelo;
+	}
+
+	public void setColorPelo(String _colorPelo) {
+		this.colorPelo = _colorPelo;
+	}
+
+	public String getTipoPelo() {
+		return this.tipoPelo;
+	}
+
+	public void setTipoPelo(String _tipoPelo) {
+		this.tipoPelo = _tipoPelo;
+	}
+
+	public String getTipoOjos() {
+		return this.tipoOjos;
+	}
+
+	public void setTipoOjos(String _tipoOjos) {
+		this.tipoOjos = _tipoOjos;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String _nombre) {
+		this.nombre = _nombre;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(int _edad) {
+		this.edad = _edad;
+	}
+
+	public String getCiudad() {
+		return this.ciudad;
+	}
+
+	public void setCiudad(String _ciudad) {
+		this.ciudad = _ciudad;
+	}
+
+	public double getPeso() {
+		return this.peso;
+	}
+
+	public void setPeso(double _peso) {
+		this.peso = _peso;
+	}
+
+	public double getAltura() {
+		return this.altura;
+	}
+
+	public void setAltura(double _altura) {
+		this.altura = _altura;
+	}
+
+}
